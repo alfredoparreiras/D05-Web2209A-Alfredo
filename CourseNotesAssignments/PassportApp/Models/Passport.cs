2dee353f3766ad5ef6066093b28469e4137a5f28
Alfredo Parreira <parreira@Alfredos-MacBook-Pro.local>
2023-02-09 20:20:33 -0500

    Passport | Passport 2 Finished

---
+++ CourseNotesAssignments/PassportApp/Models/Passport.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Passport.Models
+{
+    internal class Passport
+    {
+        // Calculated Properties 
+        public string GetFullName => string.Concat(FirstName," ",LastName);
+        //TODO: Create more accurate method. 
+        public int GetAge
+        {
+            get
+            {
+                TimeSpan age = (DateTime.Now - dateOfBirth) / 366;
+                return age.Days;
+            }
+        }
+        public string CurrentLocation
+        {
+            get
+            {
+                return travelHistory[^1].ToString();
+                //return travelHistory[travelHistory.Count - 1].ToString();
+            }
+        }
+        public bool IsTravelling
+        {
+            get
+            {
+                if (travelHistory[^1].Country != countryOfResidence)
+                    return true;
+                return false;
+            }
+        }
+        //Properties
+        public string Id
+        {
+            get
+            {
+                return id;
+            }
+        }
+        
+        // Automatic Properties 
+        private string FirstName { set; get; }
+        private string LastName { set; get; }
+        
+        // Data Fields 
+        private string id; 
+        private DateTime dateOfBirth;
+        public DateTime PassportCreation;
+        private readonly string countryOfResidence;
+        private readonly List<TravelEvent> travelHistory;
+    
+        public Passport(string firstName, string lastName, DateTime dateOfBirth, string countryOfResidence, GeneratedID generatedID)
+        {
+            id = GeneratedID.GenerateId();
+
+            //Check if first name can be used as a valid name
+            if(Validations.ValidateName(firstName))
+                this.FirstName = firstName;
+
+            //Check if last name can be used as a valid name
+            if(Validations.ValidateName(lastName))
+                this.LastName = lastName;
+
+            //Check if date of birthe can be used as a valid date;
+            if (Validations.ValidateDateOfBirth(dateOfBirth))
+                this.dateOfBirth = dateOfBirth;
+
+            //Check if country is a valid input
+            if(Validations.ValidateName(countryOfResidence))
+                this.countryOfResidence = countryOfResidence;   
+            
+            //Creating Travel Event 
+            var initialTravel = new TravelEvent(Id, this.countryOfResidence, DateTime.UtcNow);
+            
+            // Adding Travel Event 
+            travelHistory = new List<TravelEvent>();
+            travelHistory.Add(initialTravel);
+            PassportCreation = initialTravel.TimeOfEntry;
+
+        }
+        public override string ToString()
+        {
+            return $"Passport ID : {Id}\n" +
+                   $"Name : {FirstName + " " + LastName}\n" +
+                   $"Country Of Residence: {countryOfResidence}\n" +
+                   $"Current Country: {travelHistory[^1].Country}\n";
+        }
+        public void Travelling(string country, DateTime timeOfEntry)
+        {
+            var newTravel = new TravelEvent(this.Id, country, timeOfEntry);
+            travelHistory.Add(newTravel);
+        }
+    }
+}
---
+++ CourseNotesAssignments/PassportApp/Utility/Validations.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Passport.Models;
+
+namespace Passport
+{
+    internal class Validations
+    {
+        public static int ValidateInt()
+        {
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (int.TryParse(input, out int number))
+                    return number;
+                Console.WriteLine( $"Error : {input} is not a valid input.\n");
+            }
+        }
+        public static double ValidateDouble()
+        {
+            while(true)
+            {
+                string input = Console.ReadLine();
+                if (double.TryParse(input, out double number))
+                    return number;
+                Console.WriteLine($"Error : {input} is not a valid input\n");
+            }
+        }
+        public static bool ValidateName(string input)
+        {
+            if (input == null)
+                throw new ArgumentNullException("First Name is Null, please insert a Value");
+
+            else if (input.Length == 0)
+                throw new ArgumentException("First Name must be inserted");
+
+            return true;
+        }
+        public static string ValidateNameWithInput()
+        {
+            string input = Console.ReadLine();
+            
+            if (String.IsNullOrWhiteSpace(input))
+                throw new ArgumentNullException("First Name is Null, please insert a Value");
+            
+            if (input.Length == 0)
+                throw new ArgumentException("First Name must be inserted");
+            
+            return input.Replace(" ","").ToUpper();
+        }
+        public static string ValidatePassportId()
+        {
+            string input = Console.ReadLine();
+            
+            if (string.IsNullOrWhiteSpace(input))
+                throw new ArgumentNullException(string.Format("First Name{0}{1} is Null, please insert a Value", "ARG0", "ARG1"));
+            
+            if (input.Length == 0)
+                throw new ArgumentException("First Name must be inserted");
+            
+            return input.Replace(" ","");
+        }
+        public static bool ValidateDateOfBirth(DateTime input)
+        {
+            if(input.CompareTo(DateTime.UtcNow) > 0)
+                throw new ArgumentException("The date of birth needs to be earlier than today");
+
+            return true;
+        }
+        public static bool ValidateDateOfTravel(DateTime input, Models.Passport passportCheck)
+        {
+            if(input.CompareTo(passportCheck.PassportCreation) < 0)
+                throw new ArgumentException("The date of travel needs to be later than the date of passport's creation");
+        
+            return true;
+        }
+    }
+}
