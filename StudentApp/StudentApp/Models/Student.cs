47375ec4daef37ce1f37e3bc4f178abfe8357293
Alfredo Parreira <parreira@Alfredos-MacBook-Pro.local>
2023-02-09 19:47:00 -0500

    StudentApp | Finished

---
+++ StudentApp/StudentApp/Models/Student.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace WpfDemo
+{
+    public class Student
+    {
+        public int Id { get; }
+        public string Name { get; set; }
+        public int CoursesPassed { get; private set; }
+        public DateTime DateOfBirth { get; set; }
+
+        public bool Graduated
+        {
+            get
+            {
+                return graduated;
+            }
+            set
+            {
+                if (!value || CoursesPassed >= RequiredCoursesForGraduation)
+                {
+                    graduated = value;
+                }
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                DateTime today = DateTime.Today;
+                TimeSpan duration = today - DateOfBirth;
+                return (int)duration.TotalDays / 365;
+            }
+        }
+
+        private const int RequiredCoursesForGraduation = 5;
+        private bool graduated;
+
+        public Student(int id, string name, bool graduated, int coursesPassed, DateTime dateOfBirth)
+        {
+            Id = id;
+            Name = name;
+            this.graduated = graduated;
+            CoursesPassed = coursesPassed;
+            DateOfBirth = dateOfBirth;
+        }
+
+        public void PassCourse()
+        {
+            CoursesPassed++;
+        }
+    }
+}
---
+++ StudentApp/StudentApp/ViewModels/StudentListViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfDemo;
+
+namespace StudentApp.ViewModels
+{
+    public class StudentListViewModel
+    {
+        public ObservableCollection<Student> Students { get; }
+
+        public StudentListViewModel()
+        {
+            Students = new ObservableCollection<Student>();
+
+            Students.Add(new Student(1, "Alfredo Parreira", false, 5, new DateTime(1990, 09, 19)));
+            Students.Add(new Student(2, "Carolina Dias", false, 15, new DateTime(1994, 09, 22)));
+            Students.Add(new Student(3, "Danials Belzad", false, 10, new DateTime(2002, 04, 25)));
+        }
+    }
+}
