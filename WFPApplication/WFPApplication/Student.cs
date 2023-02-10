68c6ed4b23e303be4dddbc320850db4902455ea2
Alfredo Parreira <parreira@Alfredos-MacBook-Pro.local>
2023-02-09 19:47:17 -0500

    WFP Class | Synced

---
+++ WFPApplication/WFPApplication/Student.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WFPApplication
+{
+    namespace Classes
+    {
+        public class Student
+        {
+            public int Id { get; }
+            public string Name { get; set; }
+            public int CoursesPassed { get; private set; }
+            public DateTime DateOfBirth { get; }
+
+            public bool Graduated
+            {
+                get
+                {
+                    return graduated;
+                }
+                set
+                {
+                    if (!value || CoursesPassed >= RequiredCoursesForGraduation)
+                    {
+                        graduated = value;
+                    }
+                }
+            }
+
+            public int Age
+            {
+                get
+                {
+                    DateTime today = DateTime.Today;
+                    TimeSpan duration = today - DateOfBirth;
+                    return (int)duration.TotalDays / 365;
+                }
+            }
+
+            private const int RequiredCoursesForGraduation = 5;
+            private bool graduated;
+
+            public Student(int id, string name, bool graduated, int coursesPassed, DateTime dateOfBirth)
+            {
+                Id = id;
+                Name = name;
+                this.graduated = graduated;
+                CoursesPassed = coursesPassed;
+                DateOfBirth = dateOfBirth;
+            }
+
+            public void PassCourse()
+            {
+                CoursesPassed++;
+            }
+        }
+    }
+
+}
---
+++ WFPApplication/WFPApplication/StudentListViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WFPApplication.Classes;
+
+namespace WFPApplication
+{
+    public class StudentListViewModel
+    {
+        public ObservableCollection<Student> Students { get; }
+
+        public StudentListViewModel()
+        {
+            Students = new ObservableCollection<Student>();
+
+            Students.Add(new Student(1, "Alfredo Parreira", false, 3, new DateTime(1990, 09, 19)));
+            Students.Add(new Student(2, "Danials Bazhed", false, 5, new DateTime(2000, 10, 19)));
+            Students.Add(new Student(3, "Gabriela Bazhed", false, 1, new DateTime(1997, 09, 29)));
+            Students.Add(new Student(3, "Reahman :)", false, 8, new DateTime(2003, 03, 29)));
+        }
+    }
+}
